9ff0588f76e8603787b6251a4620a2cc13b45789
Harsh28Pandey <harsh28.knp@gmail.com>
2026-03-26 11:22:17 +0530

    authentication

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
+import authRouter from "./routes/auth.routes.js"
 
 const app = express()
 
 //* middlewares
 app.use(express.json())
 app.use(morgan("dev"))
+
+//* routes
+app.use("/api/auth", authRouter)
 
 export default app
