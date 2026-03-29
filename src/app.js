aff565fd91ca82156edfcd1393bf577a26e3cdb9
Harsh28Pandey <harsh28.knp@gmail.com>
2026-03-29 07:50:13 +0530

    access token and refresh token added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
 import authRouter from "./routes/auth.routes.js"
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 //* middlewares
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(cookieParser())
 
 //* routes
 app.use("/api/auth", authRouter)
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,9 +26,23 @@ export const register = async (req, res) => {
         password: hashedPassword
     })
 
-    const token = jwt.sign({
+    //* access token
+    const accessToken = jwt.sign({
         id: user._id
-    }, config.JWT_SECRET, { expiresIn: "1d" })
+    }, config.JWT_SECRET, { expiresIn: "15m" })
+
+    //* refresh token
+    const refreshToken = jwt.sign({
+        id: user._id
+    }, config.JWT_SECRET, { expiresIn: "7d" })
+
+    //* save refresh token in cookies
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000  //* 7 days
+    })
 
     res.status(201).json({
         message: "User registered successfully",
@@ -37,7 +51,7 @@ export const register = async (req, res) => {
             username: user.username,
             email: user.email
         },
-        token
+        accessToken
     })
 }
 
@@ -62,5 +76,37 @@ export const getMe = async (req, res) => {
             username: user.username,
             email: user.email
         }
+    })
+}
+
+export const refreshToken = async (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+        return res.status(401).json({
+            message: "Refresh token no found"
+        })
+    }
+
+    const decoded = jwt.verify(refreshToken, config.JWT_SECRET)
+
+    const accessToken = jwt.sign({
+        id: decoded.id
+    }, config.JWT_SECRET, { expiresIn: "15m" })
+
+    const newRefreshToken = jwt.sign({
+        id: decoded.id
+    }, config.JWT_SECRET, { expiresIn: "7d" })
+
+    res.cookie("refreshToken", newRefreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000  //* 7 days
+    })
+
+    res.status(200).json({
+        message: "Access token refreshed successfully",
+        accessToken
     })
 }
